42e4b829e1890c33934ca76ea0097a6d8726d2a7
nik <abc@example.com>
2025-05-28 11:46:13 +0400

    test file

---
+++ test.js
@@ -0,0 +1,7 @@
+const { add, subtract } = require('./calculator');
+
+// Test add
+console.log("Add 5 + 3 =", add(5, 3)); // Output: 8
+
+// Test subtract
+console.log("Subtract 5 - 3 =", subtract(5, 3)); // Output: 2
